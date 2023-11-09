653bda2ccfdd4d981ecdab3d0a58b8bef37dcbc2
DevHenriquecido <henriquem.vdossantos@gmail.com>
2023-11-09 19:57:19 -0300

    feat: creation of parking spaces

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,11 +1,43 @@
-
+import '../pages/Home.css'
 
 function Home() {
 
   
     return (
-        <section>   
-      
+        <section className='estacionamento'>   
+      <div className='vagas'>
+        1
+      </div>
+      <div className='vagas'>
+        2
+      </div>
+      <div className='vagas'>
+        3
+      </div>
+      <div className='vagas'>
+        4
+      </div>
+      <div className='vagas'>
+        5
+      </div>
+      <div className='vagas'>
+        6
+      </div>
+      <div className='vagas'>
+        7
+      </div>
+      <div className='vagas'>
+        8
+      </div>
+      <div className='vagas'>
+        9
+      </div>
+      <div className='vagas'>
+        10
+      </div>
+      <div className='vagas'>
+        11
+      </div>
           </section>
         
     )
