53a3d0887ba89c216903bf38c2e7b5dd000ac52f
DevHenriquecido <henriquem.vdossantos@gmail.com>
2023-11-09 21:51:09 -0300

    feat: api-cars-sale linking

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,24 +1,38 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import '../pages/Home.css'
 
 function Home() {
 
+const [vagas, setVagas] = useState([])
 
+
+const getVacancies = () => {
+    axios.get('http://localhost:3000/vagas')
+    .then((response) => setVagas(response.data))
+}
+
+console.log(vagas)
+
+useEffect(() => {
+    getVacancies()
+}, [])
     return (
 
         <section className='estacionamento'>   
             <ul>
-      
-            <>
-    <Link to={`/detalhes-vaga`} >
-      <div className='vagas'>
-       <li>
-        1
-        </li> 
-    </div>
-         </Link>
-                            </>
-   
+            {vagas.map((vaga) => (
+                <>
+                         <Link to={`/detalhes-vaga/${vaga.id_vaga}`}> 
+                              <div className="vagas">
+                                 
+                        {vaga.id_vaga}
+                   
+                    </div>
+                       </Link>
+                </>
+))}
     </ul>            
           </section>
         
