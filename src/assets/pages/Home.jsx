c214abab6c1ac91b3ab962cfaa91b3ab6e03c4a8
unknown <henriquem.vdossantos@gmail.com>
2023-11-12 20:51:49 -0300

    feat: adding category list styling and responsiveness

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -7,6 +7,16 @@ function Home() {
 
 const [categories, setCategories] = useState([])
 
+categories.sort(function (a, b) {
+    if (a.id_categoria > b.id_categoria) {
+      return 1;
+    }
+    if (a.id_categoria < b.id_categoria) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
 
 const getCategories = () => {
     axios.get('http://localhost:3000/categorias')
@@ -18,20 +28,25 @@ useEffect(() => {
 }, [])
 
     return (
-
-        <section className='estacionamento'>   
-            
+<section className="content">
+    <h1>Cars Sale</h1>
+    <p>Bem-vindo ao Cars Sale, aqui você pode comprar e vender o seu carro de acordo com a categoria selecionada: </p>
+    <div className='categories'>   
             {categories.map((categorie) => (
                 <>
-                         <Link to={`/categorias/${categorie.id_categoria}`}> 
-                              <div className="vagas">
-                        {categorie.id_categoria}
+                         <Link  style={{ textDecoration: 'none' }} to={`/categorias/${categorie.id_categoria}`}> 
+                              <div className="list">
+                                <span className="name">
+                        {categorie.nome_categoria}
+                                </span>
+                        <img className="image_categorie" src={categorie.foto_categoria} />
                     </div>
                        </Link>
                 </>
 ))}
+</div>
              
-          </section>
+</section>
         
     )
 }
