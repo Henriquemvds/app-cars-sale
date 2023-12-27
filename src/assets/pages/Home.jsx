4cd8adbf70b95dc89c55bb8f37c6df4fea0c8c4a
unknown <henriquem.vdossantos@gmail.com>
2023-12-27 10:43:20 -0300

    docs: project restart

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -5,46 +5,8 @@ import '../pages/Home.css'
 
 function Home() {
 
-const [categories, setCategories] = useState([])
-
-categories.sort(function (a, b) {
-    if (a.id_categoria > b.id_categoria) {
-      return 1;
-    }
-    if (a.id_categoria < b.id_categoria) {
-      return -1;
-    }
-    // a must be equal to b
-    return 0;
-  });
-
-const getCategories = () => {
-    axios.get('http://localhost:3000/categorias')
-    .then((response) => setCategories(response.data))
-}
-
-useEffect(() => {
-    getCategories()
-}, [])
-
     return (
-<section className="content">
-    <h1>Cars Sale</h1>
-    <p>Bem-vindo ao Cars Sale, aqui você pode comprar e vender o seu carro de acordo com a categoria selecionada: </p>
-    <div className='categories'>   
-            {categories.map((categorie) => (
-                <>
-                         <Link  style={{ textDecoration: 'none' }} to={`/categorias/${categorie.id_categoria}`}> 
-                              <div className="list">
-                                <span className="name">
-                        {categorie.nome_categoria}
-                                </span>
-                        <img className="image_categorie" src={categorie.foto_categoria} />
-                    </div>
-                       </Link>
-                </>
-))}
-</div>
+<section>
              
 </section>
         
