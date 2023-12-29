0e624e88230d9a8740f8664a952aa922d91aaaef
unknown <henriquem.vdossantos@gmail.com>
2023-12-29 10:17:27 -0300

    feat: purchase route

---
+++ src/assets/pages/Buy.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+
+function Buy() {
+
+  return (
+    <main className="content">
+     
+    </main>
+
+  )
+}
+
+export default Buy;
