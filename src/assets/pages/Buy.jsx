b1040ecf6248393541a54fe5c3fd38554e1f3e6e
unknown <henriquem.vdossantos@gmail.com>
2023-12-29 10:26:49 -0300

    feat: adding footer to purchase screen

--- src/assets/pages/Buy.jsx
+++ src/assets/pages/Buy.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from 'axios'
-
+import '../styles/_Home.css'
+import Footer from "../components/Footer"
 
 function Buy() {
 
   return (
     <main className="content">
-     
+       <footer className='about'>
+        <Footer />
+      </footer>
     </main>
 
   )
