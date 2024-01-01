b39ce7b9a2b9e981a9801611fc7f17ce1d2b0697
unknown <henriquem.vdossantos@gmail.com>
2024-01-01 15:55:30 -0300

    feat: Adding a footer to the car sales screen

--- src/assets/pages/Sell.jsx
+++ src/assets/pages/Sell.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from 'axios'
 import '../styles/Sell.css'
-
+import Footer from "../components/Footer"
 
 function Sell() {
 
   return (
     <main className="content">
-  
+      <footer className='about'>
+        <Footer />
+      </footer>
     </main>
 
   )
